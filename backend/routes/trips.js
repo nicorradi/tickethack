642475cf692bb9c923fbdcf12e1e750352b1afdc
nicorradi <nicorradi@hotmail.com>
2023-01-24 15:14:25 +0100

    route getTripByCityName

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -7,12 +7,19 @@ const Trip = require("../models/trips");
 /* GET home page. */
 router.get('/', function(req, res, next) {
     Trip.find().then(data => {
-        console.log('va chercher les trips');
         res.json(data);
     })
     
 });
 
+router.get('/getTripsByCityName', function(req, res, next) {
+    Trip.find({departure : req.body.cityNameDeparture, arrival : req.body.cityNameArrival}).then(data => {
+        console.log('va chercher les voyages au départ et arrivee des villes');
+        console.log(data);
+        res.json(data);
+    })
+    
+});
 
 
 module.exports = router;
