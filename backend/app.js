67f45c5de353e3fcb7ab0388ead0a1816f62bcf8
nicorradi <nicorradi@hotmail.com>
2023-01-24 12:38:43 +0100

    addRoute + yarn install

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tripsRouter = require('./routes/trips');
 
 var app = express();
+const cors = require('cors');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/trips', tripsRouter);
 
 module.exports = app;
