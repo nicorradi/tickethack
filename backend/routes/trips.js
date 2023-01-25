20c9febba9c67a1e127c5ad3a1d99c154785643e
nicorradi <nicorradi@hotmail.com>
2023-01-25 12:23:07 +0100

    script front

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -16,10 +16,10 @@ router.get("/", function (req, res, next) {
 
 
 
-router.get("/getTripsByCityName", function (req, res, next) {
+router.post("/getTripsByCityName", function (req, res, next) {
   Trip.find({
-    departure: req.body.cityNameDeparture,
-    arrival: req.body.cityNameArrival,
+    departure: req.body.departure,
+    arrival: req.body.arrival,
   }).then((data) => {
     console.log("va chercher les voyages au départ et arrivee des villes");
     console.log(data);
@@ -28,11 +28,13 @@ router.get("/getTripsByCityName", function (req, res, next) {
 });
 
 
-router.get("/getTripsByDate", function (req, res, next) {
+router.post("/getTripsByDate", function (req, res, next) {
+  
   Trip.find({
     departure: req.body.departure,
-    arrival: req.body.arrival,
+    arrival: req.body.arrival
   }).then((data) => {
+    
     let date = req.body.date;
     let filteredArray = data.filter(
       (item) => item.date.toISOString().substring(0, 10) === date
